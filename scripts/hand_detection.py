3b187d5444dd231294d72ddaf8dfe683fdaea2cd
Paulo Moraes <paulo.moraes@sou.unifal-mg.edu.br>
2025-04-09 14:00:04 -0300

    feaT: Adding a open-close based on finger raise

--- scripts/hand_detection.py
+++ scripts/hand_detection.py
@@ -1,5 +1,7 @@
 import cv2
 import mediapipe as mp
+import os
+
 
 mp_hands = mp.solutions.hands
 mp_draw = mp.solutions.drawing_utils
@@ -9,6 +11,10 @@ hands = mp_hands.Hands()
 cam = cv2.VideoCapture(0)
 cam.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
 cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
+
+NOTEPAD_IS_OPEN = False
+CHROME_IS_OPEN = False
+CALCULATOR_IS_OPEN = False
 
 def find_hand_coords(img, switch_side=False):
     rgb_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
@@ -44,12 +50,51 @@ def find_hand_coords(img, switch_side=False):
 
     return img, all_hands
 
+
+def raise_finger(hand_data):
+    finger = []
+
+    for finger_point in [8,12,16,20]:
+        if hand_data['coords'][finger_point][1] < hand_data['coords'][finger_point - 2][1]:
+            finger.append(True)
+        else:
+            finger.append(False)
+            
+    return finger
+
+
 while True:
     success, img = cam.read()
     
     img = cv2.flip(img, 1)
     
-    img, hand_data = find_hand_coords(img)  # Correctly unpack the tuple
+    img, hand_data = find_hand_coords(img) 
+
+    if len(hand_data) == 1:
+        finger_status = raise_finger(hand_data[0])
+
+        if finger_status == [True, False, False, False] and NOTEPAD_IS_OPEN == False:
+            NOTEPAD_IS_OPEN = True
+            os.startfile(r"C:\Program Files\WindowsApps\Microsoft.WindowsNotepad_11.2501.31.0_x64__8wekyb3d8bbwe\Notepad\Notepad.exe")
+
+        if finger_status == [True, True, False, False] and CHROME_IS_OPEN == False:
+            CHROME_IS_OPEN = True
+            os.startfile(r"C:\Program Files\Google\Chrome\Application\chrome.exe")
+
+        if finger_status == [True, True, True, False] and CALCULATOR_IS_OPEN == False:
+            CALCULATOR_IS_OPEN = True
+            os.startfile(r"C:\Program Files\WindowsApps\Microsoft.WindowsCalculator_11.2502.2.0_x64__8wekyb3d8bbwe\CalculatorApp.exe")
+
+        if finger_status == [False, False, False, False]:
+            NOTEPAD_IS_OPEN = False
+            # CHROME_IS_OPEN = False
+            CALCULATOR_IS_OPEN = False
+            os.system("taskkill /f /im notepad.exe")
+            # os.system("taskkill /f /im chrome.exe")
+            os.system("taskkill /f /im Calculator.exe")
+
+        if finger_status == [True, False, False, True]:
+            break
     
     cv2.imshow("Image", img)
     
